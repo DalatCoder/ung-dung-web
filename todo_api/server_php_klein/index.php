df8fbbdb215e60be3603ba25b6ee7e97991d5611
Nguyen Trong Hieu <hieuntctk42@gmail.com>
2022-05-24 16:39:35 +0700

    open cors

--- todo_api/server_php_klein/index.php
+++ todo_api/server_php_klein/index.php
@@ -7,6 +7,22 @@ require_once __DIR__ . '/vendor/autoload.php';
 $klein = new \Klein\Klein();
 $controller = new TodoController();
 
+# cors middleware
+$klein->respond('*', function ($request, $response) {
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+        header('Access-Control-Allow-Headers: token, Content-Type');
+        header('Access-Control-Max-Age: 1728000');
+        header('Content-Length: 0');
+        header('Content-Type: text/plain');
+        die();
+    }
+
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+});
+
 $klein->respond('GET', '/api/v1/todos', [$controller, 'handle_get_all_todos']);
 $klein->respond('GET', '/api/v1/todos/detail/[i:id]', [$controller, 'handle_get_todo_by_id']);
 $klein->respond('POST', '/api/v1/todos/create', [$controller, 'handle_create_new_todo']);
